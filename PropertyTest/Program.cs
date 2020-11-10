2367b676955dd41371259071720566c554c384e3
Justice Okpala <justicecharles497@gmail.com>
2020-11-10 08:24:18 +0100

    first commit

---
+++ PropertyTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PropertyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           House house = new House("jos",false,21, 0.09M);
+           House house1 = new House("plateau",false,23, 0.17M);
+           House house2 = new House("aba",false,31, 0.30M);
+
+           Bus bus = new Bus(67,9855, 9000M);
+           Bus bus1 = new Bus(75,7495, 8400M);
+           Bus bus2= new Bus(70,6555, 9000M);
+
+            List<ITaxable> taxables = new List<ITaxable>(){house, house1, house2, bus, bus1, bus2};
+
+              Console.WriteLine("\nAll Taxables Processed Polymorphically\n");
+
+            foreach (ITaxable taxable in taxables)
+            {
+                Console.WriteLine(taxable.TaxValue());
+            }
+        }
+    }
+}
+
---
+++ ShapeTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ShapeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(20.00,20.00,20.00,20.00);
+            Circle circle = new Circle(20.00,20.00,20.00);
+
+            Console.WriteLine($"{circle.Area()}");
+            Console.WriteLine($"{rectangle.Area()}");
+        }
+    }
+}
